7d85b4f4d91429e31198dd2f9db90420e14d1d57
mitchelloharawild <mitch.ohara-wild@monash.edu>
2023-11-28 13:35:57 +1100

    Update app with compare and map tab functionality

--- app.R
+++ app.R
@@ -2,7 +2,6 @@ library(shiny)
 library(ggplot2)
 library(dplyr)
 library(tidyr)
-library(lubridate)
 wpd_complete <- readRDS("data/wpd.rds")
 countries <- wpd_complete |>
   distinct(COUNTRY, REGION)
@@ -36,45 +35,50 @@ server <- function(input, output, session) {
   observe({
     regions <- na.omit(unique(wpd_complete$REGION))
     types <- na.omit(unique(wpd_complete$TYPE_BROAD))
-    updateSelectizeInput(inputId = "overall_region", choices = regions, selected = regions)
-    updateSelectizeInput(inputId = "overall_indicator", choices = c("None", names(indicators)))
-    updateSelectizeInput(inputId = "overall_type", choices = types, selected = types)
+    indicators <- paste0(names(indicators), ": ", indicators)
+    updateSelectizeInput(inputId = "region", choices = regions, selected = regions)
+    updateSelectizeInput(inputId = "indicator", choices = c("None", indicators))
+    updateSelectizeInput(inputId = "type", choices = types, selected = types)
+    updateSelectizeInput(inputId = "compare", choices = c("REGION", "COUNTRY", "TYPE_BROAD", indicators), selected = "REGION")
   })
 
   observe({
-    req(input$overall_region)
-    region_countries <- countries$COUNTRY[countries$REGION %in% input$overall_region]
-    updateSelectizeInput(inputId = "overall_country", choices = unique(wpd_complete$COUNTRY), selected = region_countries)
+    req(input$region)
+    region_countries <- countries$COUNTRY[countries$REGION %in% input$region]
+    updateSelectizeInput(inputId = "country", choices = unique(wpd_complete$COUNTRY), selected = region_countries)
   })
 
-  overall_country <- reactive({
-    input$overall_country
+  country <- reactive({
+    input$country
   })
-  country_t <- debounce(overall_country, 300)
+  country_t <- debounce(country, 300)
 
   indicator <- reactive({
-    req(input$overall_indicator)
-    if(input$overall_indicator == "None") return("Overall")
-    return(sym(input$overall_indicator))
+    req(input$indicator)
+    sub("^([^:]+).*$", "\\1", input$indicator)
+  })
+
+  indicator_sym <- reactive({
+    if(indicator() == "None") "Overall" else sym(indicator())
   })
 
   wpd_filtered <- reactive({
     req(country_t())
-    req(input$overall_type)
+    req(input$type)
     wpd_complete |>
       filter(
         COUNTRY %in% !!country_t(),
-        TYPE_BROAD %in% !!input$overall_type,
+        TYPE_BROAD %in% !!input$type,
         ROUND == 1
       )
   })
 
-  plot_data <- reactive({
+  overall_plot_data <- reactive({
     # Aggregate votes over countries
     x <- if(length(country_t()) > 1) {
-      # if(!identical(indicator(), "Overall")) browser()
+      # if(!identical(indicator_sym(), "Overall")) browser()
       wpd_filtered() |>
-        group_by(COUNTRY, INDICATOR = !!indicator(), TYPE_BROAD, YEAR = year(Date)) |>
+        group_by(COUNTRY, INDICATOR = !!indicator_sym(), TYPE_BROAD, YEAR = YEAR) |>
         filter(Date == max(Date)) |>
         # Use 0% for <5% populism elections
         replace_na(list(VOTE = 0)) |>
@@ -90,7 +94,7 @@ server <- function(input, output, session) {
         summarise(VOTE = mean(VOTE, na.rm = TRUE))
     } else {
       wpd_filtered() |>
-        group_by(COUNTRY, INDICATOR = !!indicator(), TYPE_BROAD, YEAR = Date) |>
+        group_by(COUNTRY, INDICATOR = !!indicator_sym(), TYPE_BROAD, YEAR = Date) |>
         summarise(VOTE = sum(VOTE)) |>
         group_by(COUNTRY, TYPE_BROAD) |>
         complete(YEAR, INDICATOR, fill = list(VOTE = 0)) |>
@@ -100,21 +104,21 @@ server <- function(input, output, session) {
     ungroup(x)
   })
 
-  plot_data_tot <- reactive({
-    plot_data() |>
+  overall_plot_data_tot <- reactive({
+    overall_plot_data() |>
       group_by(YEAR, TYPE_BROAD) |>
       summarise(VOTE = sum(VOTE, na.rm = TRUE))
   })
 
   output$plot_overall <- renderPlot({
-    req(plot_data_tot())
-    req(plot_data())
-    pal <- if(input$overall_indicator == "LRPOSITION") {
+    req(overall_plot_data_tot())
+    req(overall_plot_data())
+    pal <- if(indicator() == "LRPOSITION") {
       c("#A30C0C", "white", "#0D6EFD")
     } else {
       c("white", "#0D6EFD")
     }
-    p <- plot_data() |>
+    p <- overall_plot_data() |>
       ggplot(aes(y = replace_na(VOTE, 0), x = YEAR)) +
       scale_y_continuous(limits = c(-2, 100), breaks = c(5, 25, 50, 75, 100), labels = c("< 5%", "25%", "50%", "75%", "100%"), expand = expansion()) +
       scale_shape_manual(values = c("TRUE" = 1, "FALSE" = 16)) +
@@ -123,8 +127,8 @@ server <- function(input, output, session) {
       labs(x = "Year", y = "Vote (%)", linetype = NULL) +
       guides(shape = "none") +
       facet_wrap(vars(TYPE_BROAD), ncol = 1)
-    if(input$overall_indicator != "None") {
-      codebook_indicator <- paste0(input$overall_indicator, ": ", indicators[input$overall_indicator])
+    if(indicator() != "None") {
+      codebook_indicator <- paste0(indicator(), ": ", indicators[indicator()])
       caption_wrap <- session$clientData$output_plot_overall_width/12
 
       p <- p +
@@ -133,17 +137,125 @@ server <- function(input, output, session) {
         scale_fill_ordinal(type = pal) +
         labs(
           caption = paste0(strwrap(codebook_indicator, caption_wrap), collapse = "\n"),
-          fill = input$overall_indicator
+          fill = indicator()
         ) +
         theme(plot.caption = element_text(hjust = 0))
     }
     p +
-      geom_line(linetype = "dashed", data = plot_data_tot()) +
-      geom_line(aes(linetype = "Total populism", y = ifelse(VOTE == 0, NA_real_, VOTE)), data = plot_data_tot(), key_glyph = "timeseries") +
-      geom_point(size = 1.5, stroke = 0.8, data = plot_data_tot(), colour = "white") +
-      geom_point(aes(shape = VOTE == 0), size = 1.5, stroke = 0.8, data = plot_data_tot()) +
+      geom_line(linetype = "dashed", data = overall_plot_data_tot()) +
+      geom_line(aes(linetype = "Total populism", y = ifelse(VOTE == 0, NA_real_, VOTE)), data = overall_plot_data_tot(), key_glyph = "timeseries") +
+      geom_point(size = 1.5, stroke = 0.8, data = overall_plot_data_tot(), colour = "white") +
+      geom_point(aes(shape = VOTE == 0), size = 1.5, stroke = 0.8, data = overall_plot_data_tot()) +
       geom_hline(yintercept = 5, linetype = "dashed")
-  }, height = function(){max(300, length(input$overall_type)*600)}, res = 144)
+  }, height = function(){max(300, length(input$type)*600)}, res = 144)
+
+  compare <- reactive({
+    req(input$compare)
+    sub("^([^:]+).*$", "\\1", input$compare)
+  })
+
+  compare_sym <- reactive({
+    sym(compare())
+  })
+
+  compare_plot_data <- reactive({
+    # Aggregate votes over countries
+    x <- if(length(country_t()) > 1) {
+      # if(!identical(indicator_sym(), "Overall")) browser()
+      wpd_filtered() |>
+        group_by(COUNTRY, COMPARATOR = !!compare_sym(), TYPE_BROAD, YEAR = YEAR) |>
+        filter(Date == max(Date)) |>
+        # Use 0% for <5% populism elections
+        replace_na(list(VOTE = 0)) |>
+        summarise(VOTE = sum(VOTE)) |>
+        group_by(COUNTRY, TYPE_BROAD) |>
+        complete(COMPARATOR, YEAR, fill = list(VOTE = 0)) |>
+        filter(!is.na(COMPARATOR)) |>
+        group_by(COUNTRY, TYPE_BROAD, COMPARATOR) |>
+        complete(YEAR = min(YEAR):2020) |>
+        fill(VOTE) |>
+        replace_na(list(VOTE = 0)) |>
+        group_by(COMPARATOR, TYPE_BROAD, YEAR) |>
+        summarise(VOTE = mean(VOTE, na.rm = TRUE))
+    } else {
+      wpd_filtered() |>
+        group_by(COUNTRY, COMPARATOR = !!compare_sym(), TYPE_BROAD, YEAR = Date) |>
+        summarise(VOTE = sum(VOTE)) |>
+        group_by(COUNTRY, TYPE_BROAD) |>
+        complete(YEAR, COMPARATOR, fill = list(VOTE = 0)) |>
+        filter(!is.na(COMPARATOR))
+    }
+
+    ungroup(x)
+  })
+
+  output$plot_compare <- renderPlot({
+    req(compare_plot_data())
+
+    n_compare <- length(unique(compare_plot_data()$COMPARATOR))
+
+    p_aes <- if(n_compare > 8) {
+      aes(y = VOTE, x = YEAR, group = COMPARATOR)
+    } else {
+      aes(y = VOTE, x = YEAR, colour = COMPARATOR)
+    }
+    p <- compare_plot_data() |>
+      ggplot(p_aes) +
+      geom_line(aes(y = replace_na(VOTE, 2.5)), linetype = "dashed") +
+      geom_line(aes(y = VOTE)) +
+      geom_point(aes(y = replace_na(VOTE, 2.5), shape = is.na(VOTE)), size = 1.5, stroke = 1) +
+      geom_hline(yintercept = 5, linetype = "dashed") +
+      scale_y_continuous(limits = c(0, 100), breaks = c(5, 25, 50, 75, 100), labels = c("< 5%", "25%", "50%", "75%", "100%"), expand = expansion()) +
+      scale_shape_manual(values = c("TRUE" = 1, "FALSE" = 16)) +
+      theme_minimal() +
+      theme(legend.position = "bottom") +
+      labs(x = "Year", y = "Vote (%)", colour = NULL) +
+      guides(shape = "none") +
+      facet_wrap(vars(TYPE_BROAD), ncol = 1)
+
+    if(n_compare <= 8) {
+      p <- p +
+        scale_colour_brewer(type = "qual")
+    }
+    p
+  }, height = function(){max(300, 1*600)}, res = 144)
+
+  output$plot_map <- renderPlot({
+    req(wpd_filtered())
+
+    world_map <- sf::st_as_sf(maps::map('world', plot = FALSE, fill = TRUE)) |>
+      filter(ID != "Antarctica")
+    map_data <- wpd_filtered() |>
+      group_by(COUNTRY, TYPE_BROAD, YEAR = YEAR) |>
+      filter(Date == max(Date)) |>
+      # Use 0% for <5% populism elections
+      replace_na(list(VOTE = 0)) |>
+      summarise(VOTE = sum(VOTE)) |>
+      group_by(COUNTRY, TYPE_BROAD) |>
+      # complete(YEAR, fill = list(VOTE = 0)) |>
+      # group_by(COUNTRY, TYPE_BROAD) |>
+      complete(YEAR = min(YEAR):2020) |>
+      fill(VOTE) |>
+      replace_na(list(VOTE = 0)) |>
+      filter(between(as.Date(paste0(YEAR, "-01-01")), !!input$period[1], !!input$period[2])) |>
+      group_by(COUNTRY, TYPE_BROAD) |>
+      summarise(VOTE = mean(VOTE, na.rm = TRUE)) |>
+      mutate(ID = case_match(
+        COUNTRY,
+        "Czechia" ~ "Czech Republic",
+        "Türkiye" ~ "Turkey",
+        "United Kingdom" ~ "UK",
+        "United States" ~ "USA",
+        .default = COUNTRY
+      ))
+
+    inner_join(world_map, map_data, by = c("ID")) |>
+      ggplot() +
+      geom_sf(data = world_map) +
+      geom_sf(aes(fill = VOTE)) +
+      theme_minimal() +
+      facet_wrap(vars(TYPE_BROAD), ncol = 1)
+  }, height = function(){max(300, length(input$type)*600)}, res = 144)
 }
 
 # Read HTML UI and replace navbar links
